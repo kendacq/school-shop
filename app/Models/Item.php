fbb0da4ee42029e0d27d8ce91e20b4429f3b2dc9
kendacq <kenshin.dacquigan@gmail.com>
2025-09-13 01:02:06 +0800

    add item eloquent function with book

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -22,9 +22,14 @@ class Item extends Model
         'price' => 'float',
     ];
 
-    public function category() 
+    public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function book()
+    {
+        return $this->hasOne(Book::class);
     }
 
     public function variations()
