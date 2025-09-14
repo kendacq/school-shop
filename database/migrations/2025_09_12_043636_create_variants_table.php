11c1fe22737ec872a16fff4a615af4f18722d805
kendacq <kenshin.dacquigan@gmail.com>
2025-09-15 01:39:34 +0800

    refactor 'variation' to 'variant' for clarity

--- database/migrations/2025_09_12_043636_create_variants_table.php
+++ database/migrations/2025_09_12_043636_create_variants_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('variations', function (Blueprint $table) {
+        Schema::create('variants', function (Blueprint $table) {
             $table->id();
             $table->foreignId('item_id')->constrained()->cascadeOnDelete();
             $table->string('sku')->unique();
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('variations');
+        Schema::dropIfExists('variants');
     }
 };
